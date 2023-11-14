b8a279d2bab0ecff9918f3535433ec97abc7a836
AlexProstolucky <alexprostolucky@gmail.com>
2023-11-14 13:54:14 +0200

    Добавьте файлы проекта.

---
+++ RIM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "https://rymy.in.ua";
+        var web = new HtmlWeb();
+        var doc = web.Load(url);
+
+        var cssLinks = doc.DocumentNode.Descendants("link")
+            .Where(link => link.Attributes["rel"]?.Value == "stylesheet")
+            .Select(link => link.Attributes["href"]?.Value)
+            .ToList();
+
+        Console.WriteLine($"Кількість лінків на сторінці зі стилями: {cssLinks.Count}");
+
+        Console.WriteLine("Лінки на сторінці:");
+        foreach (var link in cssLinks)
+        {
+            Console.WriteLine(link);
+        }
+    }
+}
